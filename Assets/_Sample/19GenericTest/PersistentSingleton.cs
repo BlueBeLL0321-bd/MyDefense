7138a89ebf3150e8f2773d0c9dd0aa9f4373622e
DESKTOP-5V3FRMH\M <leeyongwoo736@gmail.com>
2025-04-29 16:46:48 +0900

    PersistentSingleton 스크립트 수정

--- Assets/_Sample/19GenericTest/PersistentSingleton.cs
+++ Assets/_Sample/19GenericTest/PersistentSingleton.cs
@@ -5,7 +5,7 @@ namespace Sample.Generic
     // 제네릭 싱글톤 클래스 : 신 전환 시 파괴되지 않는다
     public class PersistentSingleton<T> : Singleton<T> where T : Singleton<T>
     {
-        protected virtual void Awake()
+        protected override void Awake()
         {
             base.Awake();
 
